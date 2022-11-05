bd17c32c0be184de5ee40bea1c8dbf499f4f1197
JaryJay <jay.jrjren@gmail.com>
2022-11-05 15:52:29 -0400

    Added clock

--- better-clock/src/App.js
+++ better-clock/src/App.js
@@ -1,10 +1,20 @@
-import Clock from './components/Clock.jsx'
+import React from 'react';
+import Clock from './components/Clock.jsx';
+import clockImage from './clock.png';
 
 function App() {
   return (
-    <div className="App">
-      <h1 class='text-lg text-center'>jay is a monkey</h1>
-    </div>
+    <body className="h-screen pt-3 bg-gradient-to-b from-green-200 to-green-500">
+      <div className="text-center">
+        <h1 className='text-5xl' style={{ color: '#8fa35f', font: "Poppins", paddingBottom: '15px' }} > BETTER - CLOCK</h1>
+        <h2 className='text-3xl'>the better, base-10 time system</h2>
+        <div className='relative'>
+          <img src={clockImage} alt="clock" className='relative' width='30%'></img>
+        </div>
+        <Clock />
+        <h2 class='text-2xl mt-5'>most things in our lives from temperature to measurement are base-10. why not time?</h2>
+      </div>
+    </body >
   );
 }
 
--- better-clock/src/components/Clock.jsx
+++ better-clock/src/components/Clock.jsx
@@ -1,7 +1,38 @@
 const Clock = ({ props }) => {
-	return (
-		<div>
 
+	const milliseconds = new Date().getTime();
+	const now = new Date().getTime();
+
+
+	setInterval(function () {
+		const hours = new Date().getHours();
+		const minutes = new Date().getMinutes();
+		const seconds = new Date().getSeconds();
+		const millisesconds = new Date().getMilliseconds();
+		const currentMillis = (hours * 3600 + minutes * 60 + seconds) * 1000 + millisesconds;
+		const seans = Math.floor((currentMillis - (currentMillis % 8640000)) / 8640000);
+		const ceras = Math.floor((currentMillis - seans * 8640000) / 86400);
+		const jays = Math.floor((currentMillis - seans * 8640000 - ceras * 86400) / 864);
+		const percentage = Math.floor((currentMillis / 86400000) * 1000)
+
+		document.getElementById("test").innerHTML = seans + ":" + ceras + ":" + jays;
+	}, 864);
+
+	setInterval(function () {
+		const hours = new Date().getHours() % 12;
+		const minutes = new Date().getMinutes();
+		const seconds = new Date().getSeconds();
+		document.getElementById("clock").innerHTML = hours + ":" + minutes + ":" + seconds;
+	}, 1000)
+
+	return (
+		<div class='text-3xl mt-5'>
+			<div>
+
+			</div>
+			Normal Clock: <span id='clock'>0:00:00</span>
+			<br />
+			Better Clock: <span id='test'>0:00:00</span>
 		</div>
 	);
 };
